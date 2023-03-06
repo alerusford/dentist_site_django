35e3dc93051059dc4eaedf0ee4d3c74d3601e1cd
alexandr <alerusford@gmail.com>
2023-03-06 17:33:17 +0300

    html+django home-ok

--- website/views.py
+++ website/views.py
@@ -1,4 +1,33 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, 'home.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+
+def appointment(request):
+    return render(request, 'appointment.html', {})
+
+
+def contact(request):
+    return render(request, 'contact.html', {})
+
+
+def price(request):
+    return render(request, 'price.html', {})
+
+
+def service(request):
+    return render(request, 'service.html', {})
+
+
+def team(request):
+    return render(request, 'team.html', {})
+
+
+def testimonial(request):
+    return render(request, 'testimonial.html', {})
